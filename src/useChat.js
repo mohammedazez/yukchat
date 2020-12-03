835413cdb4bf46f60489cbaf0f2a9239cbc653a1
azezmohammed <muhamadaziz047@gmail.com>
2020-12-03 15:21:49 +0700

    update useChat

--- src/useChat.js
+++ src/useChat.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+// Import socket Io Client
+import socketIOClient from "socket.io-client";
 
-function useChat() {
+// Name of the event
+const EVENT_CHAT_PESAN_BARU = "obrolanPesanBaru";
+const SOCKET_SERVER_URL = "http://localhost:3000";
+
+function useChat(roomId) {
+  // (useState) Kirim dan menerima pesan
+  const [pesan, setPesan] = useState([]);
+  const socketRef = useRef();
+
+  useEffect(() => {
+    //  Menyambungkan koneksi untuk Web Socket
+    socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
+      query: { roomId },
+    });
+  });
+
+  //  Mendengarkan pesan masuk
+  socketRef.current.on(EVENT_CHAT_PESAN_BARU, (pesan) => {
+    const pesanMasuk = {
+      ...pesan,
+      dimilikoOlehPenggunaSaatIni: pesan.senderId === socketRef.current.id,
+    };
+    setPesan((pesan) => [...pesan, pesanMasuk]);
+  });
+  // (useEffect) Ketika koneksi tertutup
+
+  //   mengirim pesan ke server lalu diteruskan ke semua pengguna di ruangan chat yang sama
+
   return (
     <div>
       <h1>Use Chat</h1>
