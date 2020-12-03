d9eadcb2e7deb3c3dc341709c8f2e086e526b091
azezmohammed <muhamadaziz047@gmail.com>
2020-12-03 11:23:53 +0700

    progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Components
+import Home from "./components/Home";
+import ChatRoom from "./components/ChatRoom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Ini app js</p>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/:roomId" component={ChatRoom} />
+          </Switch>
+        </Router>
       </header>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 
 function Home() {
+  const [roomName, setRoomName] = useState("");
+
+  const handleRoomNameChange = (event) => {
+    setRoomName(event.target.value);
+  };
+
   return (
-    <div>
-      <h1>Ini home</h1>
+    <div className="home-container">
+      <input
+        type="text"
+        placeholder="Room"
+        className="text-input-field"
+        value={roomName}
+        onChange={handleRoomNameChange}
+      />
+      <Link to={`/${roomName}`} className="enter-room-button">
+        Join room
+      </Link>
     </div>
   );
 }
