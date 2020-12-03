c1a5ee124b04654ce1fe4d85e70292715e1499f8
azezmohammed <muhamadaziz047@gmail.com>
2020-12-03 14:36:15 +0700

    add handlechange

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ChatRoom.css";
 
 function ChatRoom() {
+  const [pesanBaru, setPesanBaru] = useState("");
+
+  const handlepesanBaruBerubah = (event) => {
+    setPesanBaru(event.target.value);
+    // console.log(handlepesanBaruBerubah);
+  };
+
   return (
     <div className="chat-room-container">
       <h1 className="room-name">Room: </h1>
       <textarea
-        placeholder="Write message..."
+        value={pesanBaru}
+        onChange={handlepesanBaruBerubah}
+        placeholder="Ketik pesan disini ya!!!"
         className="new-message-input-field"
       />
-      <button>Kirim pesan</button>
+      <button className="send-message-button">Kirim pesan</button>
     </div>
   );
 }
